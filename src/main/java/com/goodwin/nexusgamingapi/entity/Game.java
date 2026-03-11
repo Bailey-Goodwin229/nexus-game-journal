28d6e6a37f9c75d4d940a3f3a7313a4f7bea454f
Bailey <bailey.goodwin229@yahoo.com>
2026-03-10 21:28:46 -0400

    feat: create Game entity with JPA mapping

--- src/main/java/com/goodwin/nexusgamingapi/entity/Game.java
+++ src/main/java/com/goodwin/nexusgamingapi/entity/Game.java
@@ -1,4 +1,20 @@
 package com.goodwin.nexusgamingapi.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Entity
+@Table(name = "games")
+@Data
 public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    private String summary;
+
 }
