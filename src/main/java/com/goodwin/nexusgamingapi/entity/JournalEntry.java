255e04fdf8a2ecc774651e218606d28b06290a74
Bailey <bailey.goodwin229@yahoo.com>
2026-03-17 16:01:48 -0400

    feat: create JournalEntry entity and database table

---
+++ src/main/java/com/goodwin/nexusgamingapi/entity/JournalEntry.java
@@ -0,0 +1,34 @@
+package com.goodwin.nexusgamingapi.entity;
+
+// Entity class for the journal entry information
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JournalEntry {
+
+    // variables for the entity
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long journalId;
+
+    private int ratings;
+
+    @Column(columnDefinition = "TEXT")
+    private String notes;
+
+    private LocalDate entryDate;
+
+    // Connects entry to a specific game
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+}
